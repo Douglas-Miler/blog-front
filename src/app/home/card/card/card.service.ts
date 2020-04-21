6fd7a60604a19fc90c67a4eea2944c31af3312de
Douglas Miler <douglas.junior@bertini.com.br>
2020-04-21 20:45:00 -0300

    apontando pra producao

--- src/app/home/card/card/card.service.ts
+++ src/app/home/card/card/card.service.ts
@@ -15,7 +15,7 @@ export class CardService {
 
     listCardsPaginated(page: number): Observable<Card[]>{
         const params = new HttpParams().append('page', page.toString());
-        return this.http.get<Card[]>('http://localhost:8080/cards', { params: params });
+        return this.http.get<Card[]>('https://web-api-blog.herokuapp.com/cards', { params: params });
     }
 
 }
