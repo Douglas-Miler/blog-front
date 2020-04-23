009f82ca4ec09581a26980cc4d5197fe7c1d5c36
Douglas Miler <douglas.junior@bertini.com.br>
2020-04-23 03:35:54 -0300

    SignUp Form

--- src/app/author/navbar/modal/new-user/new-user.component.ts
+++ src/app/author/navbar/modal/new-user/new-user.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'blog-new-user',
     templateUrl: './new-user.component.html'
 })
-export class NewUserComponent{
+export class NewUserComponent implements OnInit{
 
     hide: boolean = true;
 
+    constructor(private dialog: MatDialogRef<NewUserComponent>){
+
+    }
+
+    ngOnInit(): void {
+        this.dialog.updateSize('600px').updatePosition({top: '200px'});
+    }
 }
